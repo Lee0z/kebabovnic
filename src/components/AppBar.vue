d50e28466136c531e6e48b6c6569965e3b7272af
Lee0z <leeos10100@gmail.com>
2025-01-10 21:07:20 +0100

    Add Login/Register modal and integrate with AppBar for user authentication

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,24 +1,28 @@
 <template>
-  <nav class="app-bar p-4 top-0 z-50 flex items-center shadow-md">
-    <div class="flex-1 flex justify-center">
-      <div class="logo text-2xl md:text-5xl font-bold">Kebabovnic</div>
+  <nav class="app-bar p-4 bg-white fixed w-full top-0 z-50 flex items-center shadow-md">
+    <div class="logo absolute left-1/2 transform -translate-x-1/2 text-xl md:text-3xl font-bold">
+      Kebabovnic
     </div>
-    <div class="nav-links flex items-center space-x-4 text-lg absolute right-4">
-      <RouterLink to="/" class="flex items-center space-x-2">
-        <HomeIcon class="w-6 h-6" />
+    <div class="nav-links flex items-center space-x-2 text-base md:text-lg ml-auto">
+      <RouterLink to="/" class="flex items-center space-x-1 md:space-x-2">
+        <HomeIcon class="w-5 h-5 md:w-6 md:h-6" />
         <span>Home</span>
       </RouterLink>
-      <RouterLink to="/search" class="flex items-center space-x-2">
-        <MagnifyingGlassIcon class="w-6 h-6" />
+      <RouterLink to="/search" class="flex items-center space-x-1 md:space-x-2">
+        <MagnifyingGlassIcon class="w-5 h-5 md:w-6 md:h-6" />
         <span>List kebabs</span>
       </RouterLink>
+      <button class="flex items-center space-x-1 md:space-x-2" @click="$emit('open-modal')">
+        <UserIcon class="w-5 h-5 md:w-6 md:h-6" />
+        <span>Menu</span>
+      </button>
     </div>
   </nav>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router'
-import { HomeIcon, Bars3Icon, MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
+import { HomeIcon, UserIcon, MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,29 +4,39 @@ import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
 onMounted(() => {
-  const map = L.map('map').setView([51.505, -0.09], 13)
+  const map = L.map('map').setView([51.207, 16.161], 13)
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map)
 
-  L.marker([51.505, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup()
+  window.addEventListener('resize', () => {
+    map.invalidateSize()
+
+  })
 })
 </script>
 
 <template>
-  <main class="flex flex-col items-center">
-    <h1 class="text-4xl font-bold text-center my-8">Home</h1>
-    <div class="map-container w-full max-w-4xl mx-auto">
-      <div id="map" class="h-96 w-full"></div>
+  <main class="flex flex-col items-center flex-grow h-screen pt-16 w-full">
+    <div class="map-container flex-grow w-full max-w-4xl mx-auto">
+      <div id="map" class="h-full w-full"></div>
     </div>
   </main>
 </template>
 
 <style scoped>
 .map-container {
+  flex-grow: 1;
+  max-width: 100%;
+  max-height: 100%;
+  position: relative;
+}
+
+#map {
   height: 100%;
+  width: 100%;
+  z-index: 1;
+  background-color: #1a1a1a;
 }
 </style>
