37ef2afa6e58f8fa1d4b132c388a7f9edb7a399c
Lee0z <leeos10100@gmail.com>
2025-01-20 18:00:31 +0100

    add models for Sauce and Filling; implement filter component and enhance HomeView with modal support

---
+++ src/models/SauceModel.js
@@ -0,0 +1,11 @@
+class Sauce {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.hexColor = data.hex_color;
+    this.createdAt = data.created_at;
+    this.updatedAt = data.updated_at;
+  }
+}
+
+export default Sauce;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,59 @@
 <script setup>
-import { onMounted } from 'vue'
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import { get } from '@/utils/api'
-import KebabPlace from '@/models/KebabPlaceModel'
-import Pusher from 'pusher-js'
+import { ref, onMounted } from 'vue';
+import { get } from '@/utils/api';
+import KebabPlace from '@/models/KebabPlaceModel';
+import KebabPlaceDetails from '@/components/KebabPlaceDetails.vue';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import Pusher from 'pusher-js';
 
-onMounted(async () => {
-  const map = L.map('map').setView([51.207, 16.161], 13)
+const map = ref(null);
+const kebabPlaces = ref([]);
+const selectedKebabPlace = ref(null);
+const isModalOpen = ref(false);
 
+const fetchKebabPlaces = async () => {
+  try {
+    const data = await get('/kebab-places?paginate=1000');
+    kebabPlaces.value = data.data.map(place => new KebabPlace(place));
+    kebabPlaces.value.forEach(addKebabPlaceToMap);
+  } catch (error) {
+    console.error('Error fetching kebab places:', error);
+  }
+};
+
+const addKebabPlaceToMap = (kebabPlace) => {
+  const { latitude, longitude } = kebabPlace.getCoordinates();
+  const marker = L.marker([latitude, longitude]).addTo(map.value)
+
+  marker.on('click', () => {
+    openModal(kebabPlace);
+  });
+};
+
+const openModal = (kebabPlace) => {
+  selectedKebabPlace.value = kebabPlace;
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+  selectedKebabPlace.value = null;
+};
+
+onMounted(() => {
+  map.value = L.map('map').setView([51.207, 16.161], 13);
   L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map)
+  }).addTo(map.value);
 
-  const addKebabPlaceToMap = (kebabPlace) => {
-    const { latitude, longitude } = kebabPlace.getCoordinates()
-    L.marker([latitude, longitude]).addTo(map)
-      .bindPopup(`<b>${kebabPlace.name}</b><br>${kebabPlace.address}`)
-  }
-
-  try {
-    const response = await get('/kebab-places')
-    const kebabPlaces = response.data.map(place => new KebabPlace(place))
-
-    kebabPlaces.forEach(kebabPlace => {
-      addKebabPlaceToMap(kebabPlace)
-    })
-  } catch (error) {
-    console.error('Error fetching kebab places:', error)
-  }
+  fetchKebabPlaces();
 
   const pusher = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
     wsHost: import.meta.env.VITE_WEBSOCKET_DOMAIN,
   });
-
 
   const channel = pusher.subscribe('kebab-places');
 
@@ -46,9 +64,9 @@ onMounted(async () => {
   });
 
   window.addEventListener('resize', () => {
-    map.invalidateSize()
-  })
-})
+    map.value.invalidateSize();
+  });
+});
 </script>
 
 <template>
@@ -56,6 +74,7 @@ onMounted(async () => {
     <div class="map-container flex-grow w-full max-w-4xl mx-auto">
       <div id="map" class="h-full w-full"></div>
     </div>
+    <KebabPlaceDetails :isOpen="isModalOpen" :onRequestClose="closeModal" :kebabPlace="selectedKebabPlace" class="z-50" />
   </main>
 </template>
 
